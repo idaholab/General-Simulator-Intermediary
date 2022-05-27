fdd05da3f09418dad419edf5cb632662000a5506
Jacob Lehmer <Jacob.Lehmer@inl.gov>
2022-05-27 09:14:42 -0600

    Project creation completed, git repository functional
    Signed-off-by: Jacob Lehmer <Jacob.Lehmer@inl.gov>

---
+++ GeneralizedSimulatorIntermediary/GeneralizedSimulatorIntermediary/GeneralizedSimulatorIntermediary.cpp
@@ -0,0 +1,12 @@
+﻿// GeneralizedSimulatorIntermediary.cpp : Defines the entry point for the application.
+//
+
+#include "GeneralizedSimulatorIntermediary.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Technically the first CMake program that I have written." << endl;
+	return 0;
+}
